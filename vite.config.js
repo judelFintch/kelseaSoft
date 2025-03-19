6c177c36634ec5a01466af47e5c2d06a2c2ceb33
judel fintch <judel@gmail.com>
2025-03-19 11:22:21 +0200

    feat:adding reaod map

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from "@tailwindcss/vite";
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/css/style.css',
-                'resources/js/index.js'
-            ],
-            refresh: ['resources/views/**/*'],
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-    },
+  plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "jsvectormap/dist/css/jsvectormap.min.css";`
+      }
+    }
+  }
 });
