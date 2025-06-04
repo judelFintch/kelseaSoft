4fa3b2bbd6a3e9296309ccd596a9915903883c7c
judel fintch <judel@gmail.com>
2025-06-04 16:15:03 +0200

    adding acronyn in componi form

--- resources/views/livewire/admin/company/company-create.blade.php
+++ resources/views/livewire/admin/company/company-create.blade.php
@@ -32,6 +32,21 @@
                                     <span class="text-sm text-red-600">{{ $message }}</span>
                                 @enderror
                             </div>
+
+                            <!-- Acronym -->
+                            <div>
+                                <label for="acronym"
+                                    class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                                    Acronym
+                                </label>
+                                <input type="text" id="acronym" wire:model.defer="acronym"
+                                    placeholder="Enter company acronym"
+                                    class="dark:bg-dark-900 shadow-theme-xs focus:border-indigo-300 focus:ring-indigo-500/10 dark:focus:border-indigo-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-none dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" />
+                                @error('acronym')
+                                    <span class="text-sm text-red-600">{{ $message }}</span>
+                                @enderror
+                            </div>
+
                             <!-- Business Category -->
                             <div>
                                 <label for="business_category"
